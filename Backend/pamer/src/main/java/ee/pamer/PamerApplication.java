4cc9d663c8a9a6bf2e206c8eb37c8cdb0a661964
rizaozer <riazozer@gmail.com>
2023-10-01 12:36:09 +0300

    initial commit

---
+++ Backend/pamer/src/main/java/ee/pamer/PamerApplication.java
@@ -0,0 +1,13 @@
+package ee.pamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PamerApplication.class, args);
+	}
+
+}
---
+++ Backend/pamer/src/test/java/ee/pamer/PamerApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.pamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
