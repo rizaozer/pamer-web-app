f36d0512c298fdc2bae1f00299423c906debbe76
rizaozer <riazozer@gmail.com>
2023-10-10 21:27:58 +0300

    Update StringEncryptor and add multihttpsecurityconfig

---
+++ Backend/pamer/src/main/java/ee/pamer/security/MultiHttpSecurityConfig.java
@@ -0,0 +1,43 @@
+package ee.pamer.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class MultiHttpSecurityConfig {
+
+    @Configuration
+    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        @Autowired
+        private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+        @Autowired
+        private CustomUserDetailsService userDetailsService;
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                    .userDetailsService(userDetailsService)
+                    .passwordEncoder(bCryptPasswordEncoder);
+        }
+
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .csrf()
+                    .disable()
+                    .antMatcher("/api/**")
+                    .authorizeRequests()
+                    .antMatchers("/api/v1/user/signup").permitAll()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .exceptionHandling()
+                    .authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                    .and()
+                    .addFilter(new ApiJWTAuthenticationFilter(authenticationManager()))
+                    .addFilter(new ApiJWTAuthorizationFilter(authenticationManager()))
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        }
+    }
+}
--- Backend/pamer/src/main/java/ee/pamer/security/MyStringEncryptor.java
+++ Backend/pamer/src/main/java/ee/pamer/security/MyStringEncryptor.java
@@ -4,11 +4,26 @@ import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.exceptions.EncryptionOperationNotPossibleException;
 import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.stereotype.Component;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
 @Component
 public class MyStringEncryptor implements StringEncryptor {
 
     private final BasicTextEncryptor delegate;
+
+
+    public String encryptProperty(String property, String secretKey) {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword(secretKey);
+        return encryptor.encrypt(property);
+    }
+
+    public String decryptProperty(String encryptedProperty, String secretKey) {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword(secretKey);
+        return encryptor.decrypt(encryptedProperty);
+    }
+
 
     public MyStringEncryptor(String secretKey) {
         delegate = new BasicTextEncryptor();
